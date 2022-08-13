8c1db51983295baa5a3c47353a8ecd4db90509fb
ericknathan <erick.capito@hotmail.com>
2022-08-13 18:07:55 -0300

    feat: ensure RemoteAuthentication calls HttpPostClient with correct body

--- src/data/protocols/http/http-post-client.ts
+++ src/data/protocols/http/http-post-client.ts
@@ -1,12 +1,11 @@
 export interface HttpPostClient {
-  url?: string;
   post(params: HttpPostClient.Params): Promise<HttpPostClient.Result>;
 }
 
 export namespace HttpPostClient {
   export type Params = {
     url: string;
-    body?: any;
+    body?: object;
   }
 
   export type Result = void;
