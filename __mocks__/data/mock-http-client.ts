924134e974944aa1de7f75ae664bf3aa5dd03b48
ericknathan <erick.capito@hotmail.com>
2022-08-16 21:25:18 -0300

    test: ensure RemoteAuthentication throws InvalidCredentialsError on 401

--- __mocks__/data/mock-http-client.ts
+++ __mocks__/data/mock-http-client.ts
@@ -1,11 +1,15 @@
-import { HttpPostClient } from '@/data/protocols/http/http-post-client';
+import { HttpPostClient, HttpStatusCode } from '@/data/protocols/http';
 
 export class HttpClient implements HttpPostClient {
   url?: string;
   body?: object;
+  response: HttpPostClient.Result = {
+    statusCode: HttpStatusCode.NO_CONTENT,
+  }
 
   async post(params: HttpPostClient.Params): Promise<HttpPostClient.Result> {
     this.url = params.url;
     this.body = params.body;
+    return Promise.resolve(this.response);
   }
 }
--- __tests__/data/usecases/authentication/remote-authentication.spec.ts
+++ __tests__/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,7 +1,10 @@
+import { faker } from '@faker-js/faker/locale/pt_BR';
+
 import { RemoteAuthentication } from '@/data/usecases/authentication/remote-authentication';
 import { HttpClient } from '@/mocks/data/mock-http-client';
 import { mockAuthentication } from '@/mocks/domain/mock-authentication';
-import { faker } from '@faker-js/faker/locale/pt_BR';
+import { InvalidCredentialsError } from '@/domain/errors';
+import { HttpStatusCode } from '@/data/protocols/http';
 
 type SutTypes = {
   sut: RemoteAuthentication;
@@ -31,5 +34,14 @@ describe('RemoteAuthentication', () => {
     const authenticationParams = mockAuthentication();
     await sut.auth(authenticationParams);
     expect(httpPostClientSpy.body).toEqual(authenticationParams)
+  });
+  
+  it('should throw InvalidCredentialsError if HttpClient returns 401', async () => {
+    const { sut, httpPostClientSpy } = makeSut();
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.UNAUTHORIZED,
+    }
+    const promise = sut.auth(mockAuthentication());
+    await expect(promise).rejects.toThrow(new InvalidCredentialsError())
   });
 });
