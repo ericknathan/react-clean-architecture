225dfe548ae159dffbff6f7b30b212e8800f300f
ericknathan <erick.capito@hotmail.com>
2022-08-18 08:59:35 -0300

    feat: ensure Login will not load spinner on start

--- src/presentation/pages/login/components/form/index.tsx
+++ src/presentation/pages/login/components/form/index.tsx
@@ -1,15 +1,26 @@
-import { Button, Input } from '@/presentation/components';
-import React from 'react';
+import React, { useState } from 'react';
+
+import { Button, Input, Spinner } from '@/presentation/components';
 
 import styles from './login-form.module.scss';
 
+type StateProps = {
+  isLoading: boolean;
+}
+
 export function LoginForm() {
+  const [formStates, setFormStates] = useState<StateProps>({
+    isLoading: false
+  });
+  
   return (
     <form className={styles.form} action="submit">
       <h2>Realizar login</h2>
       <Input type="email" name="email" placeholder="Digite seu e-mail" />
       <Input type="password" name="password" placeholder="Digite sua senha" />
-      <Button className={styles.submitButton}>Entrar</Button>
+      <Button className={styles.submitButton}>
+        {formStates.isLoading ? <Spinner /> : 'Entrar'}
+      </Button>
       <a href="#" className={styles.createAccountButton}>Não possui um cadastro? Criar conta</a>
     </form>
   )
