4575a78f8d07af34c725474e83663cbf70ff2101
ericknathan <erick.capito@hotmail.com>
2022-08-18 09:14:03 -0300

    test: ensure Login page starts with inputs with required state

--- __tests__/presentation/pages/login.spec.tsx
+++ __tests__/presentation/pages/login.spec.tsx
@@ -4,9 +4,16 @@ import { Login } from '@/presentation/pages/login';
 
 describe('Login Page', () => {
   it('should start with initial state', async () => {
-    const { queryByText } = render(<Login />);
+    const { queryByText, queryByTestId } = render(<Login />);
+
     const submitButton = queryByText('Entrar') as HTMLButtonElement;
     expect(submitButton).toBeTruthy();
-    expect(submitButton.disabled).toBe(true);
+    expect(submitButton.disabled).toBeTruthy();
+
+    const emailInput = queryByTestId('email-input') as HTMLInputElement;
+    expect(emailInput.required).toBeTruthy();
+
+    const passwordInput = queryByTestId('password-input') as HTMLInputElement;
+    expect(passwordInput.required).toBeTruthy();
   })
 })
