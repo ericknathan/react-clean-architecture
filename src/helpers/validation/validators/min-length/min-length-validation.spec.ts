2e494cdae63bcc1e8bd093ceb0a36b03c1f10c7c
ericknathan <erick.capito@hotmail.com>
2022-08-19 11:38:52 -0300

    refactor: change validation values to use jest

--- src/helpers/validation/validators/min-length/min-length-validation.spec.ts
+++ src/helpers/validation/validators/min-length/min-length-validation.spec.ts
@@ -7,13 +7,13 @@ const makeSut = () => new MinLengthValidation(faker.database.column(), 5);
 describe('MinLengthValidation', () => {
   it('should return error if value length is invalid', () => {
     const sut = makeSut();
-    const error = sut.validate('123');
+    const error = sut.validate(faker.random.alphaNumeric(sut.minLength - 1));
     expect(error).toEqual(new InvalidFieldError(sut.field, `O valor deve ter no mínimo ${sut.minLength} caracteres`));
   });
 
   it('should return falsy if value length is valid', () => {
     const sut = makeSut();
-    const error = sut.validate('12345');
+    const error = sut.validate(faker.random.alphaNumeric(sut.minLength));
     expect(error).toBeFalsy();
   });
 });
