cc6d64a3880aac658e1cfea01ae4f1e81f901738
ericknathan <erick.capito@hotmail.com>
2022-08-19 11:41:03 -0300

    fix: ensure EmailValidation allows empty value

--- src/helpers/validation/validators/email/email-validation.spec.ts
+++ src/helpers/validation/validators/email/email-validation.spec.ts
@@ -9,11 +9,17 @@ describe('EmailValidation', () => {
     const sut = makeSut();
     const error = sut.validate(faker.random.word());
     expect(error).toEqual(new InvalidFieldError('email'));
-  })
+  });
 
   it('should return falsy if email is valid', () => {
     const sut = makeSut();
     const error = sut.validate(faker.internet.email());
     expect(error).toBeFalsy();
-  })
-})
+  });
+
+  it('should return falsy if email is empty', () => {
+    const sut = makeSut();
+    const error = sut.validate('');
+    expect(error).toBeFalsy();
+  });
+});
--- src/helpers/validation/validators/email/email-validation.ts
+++ src/helpers/validation/validators/email/email-validation.ts
@@ -4,6 +4,6 @@ import { InvalidFieldError } from "@/helpers/validation/errors";
 export class EmailValidation implements FieldValidation {
   validate(value: string): Error | null {
     const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    return emailRegex.test(value) ? null : new InvalidFieldError('email');
+    return (!value || emailRegex.test(value)) ? null : new InvalidFieldError('email');
   }
 }
