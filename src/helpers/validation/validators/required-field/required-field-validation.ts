b5881ca03d7ed48c5d09c640f2eadf8651296f8a
ericknathan <erick.capito@hotmail.com>
2022-08-19 11:03:45 -0300

    feat: ensure RequiredFieldValidation returns error if field is empty

---
+++ src/helpers/validation/validators/required-field/required-field-validation.ts
@@ -0,0 +1,12 @@
+import { FieldValidation } from "@/helpers/validation/protocols";
+import { RequiredFieldError } from "@/helpers/validation/errors";
+
+export class RequiredFieldValidation implements FieldValidation {
+  constructor (
+    readonly field: string
+  ) {}
+
+  validate(value: string): Error | null {
+    return new RequiredFieldError();
+  }
+}
