483c73f61abef0e169ae29b7846d54f9aa348c0c
ericknathan <erick.capito@hotmail.com>
2022-08-19 11:08:27 -0300

    refactor: change faker locale on Login page tests

--- __tests__/presentation/pages/login.spec.tsx
+++ __tests__/presentation/pages/login.spec.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from "history";
 import { cleanup, fireEvent, render, RenderResult, waitFor } from '@testing-library/react';
-import { faker } from '@faker-js/faker';
+import { faker } from '@faker-js/faker/locale/pt_BR';
 import 'jest-localstorage-mock';
 
 import { Login } from '@/presentation/pages';
