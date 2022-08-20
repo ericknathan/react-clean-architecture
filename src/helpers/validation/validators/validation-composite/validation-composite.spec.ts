4e2d1d3b54906738c5176df29a3811dc3cc7b96f
ericknathan <erick.capito@hotmail.com>
2022-08-20 15:32:13 -0300

    refactor: move SUT creation to a factory method

--- src/helpers/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/helpers/validation/validators/validation-composite/validation-composite.spec.ts
@@ -3,21 +3,35 @@ import { faker } from '@faker-js/faker/locale/pt_BR';
 import { FieldValidationStub } from '@/mocks/helpers';
 import { ValidationComposite } from '@/helpers/validation/validators';
 
+type SutTypes = {
+  sut: ValidationComposite;
+  fieldValidationStubs: FieldValidationStub[];
+  fieldName: string;
+}
+
+const makeSut = (fieldName = faker.database.column(), fieldValidationStubs = [
+  new FieldValidationStub(fieldName),
+  new FieldValidationStub(fieldName),
+]): SutTypes => {
+  const sut = new ValidationComposite(fieldValidationStubs);
+
+  return {
+    sut,
+    fieldValidationStubs,
+    fieldName
+  };
+}
+
 describe('ValidationComposite', () => {
   it('should return error if any validation fails', () => {
-    const fieldName = faker.database.column();
-    const validationStub = new FieldValidationStub(fieldName);
-    validationStub.error = new Error(faker.random.words());
-    const validationStub2 = new FieldValidationStub(fieldName);
-    validationStub2.error = new Error(faker.random.words());
-    const sut = new ValidationComposite([
-      validationStub,
-      validationStub2,
-    ]);
+    const { sut, fieldValidationStubs, fieldName } = makeSut();
+    fieldValidationStubs[0].error = new Error(faker.random.words());
+    fieldValidationStubs[1].error = new Error(faker.random.words());
+    
     const error = sut.validate({
       fieldName,
       fieldValue: faker.random.word(),
     });
-    expect(error).toBe(validationStub.error.message);
+    expect(error).toBe(fieldValidationStubs[0].error.message);
   })
 })
