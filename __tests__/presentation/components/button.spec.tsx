56b1c72d9fb423f5cdfec38bba941bbccce07f9b
ericknathan <erick.capito@hotmail.com>
2022-08-20 18:10:25 -0300

    refactor: move SUT creation to a factory method

--- __tests__/presentation/components/button.spec.tsx
+++ __tests__/presentation/components/button.spec.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { Button } from '@/presentation/components';
 
+const makeSut = () => render(<Button>example</Button>);
+
 describe('Button Component', () => {
   test('should begin with empty className', () => {
-    const { getByTestId } = render(<Button>example</Button>);
+    const { getByTestId } = makeSut();
     const button = getByTestId('button');
     expect(button.className).not.toContain('undefined');
   });
--- __tests__/presentation/components/input.spec.tsx
+++ __tests__/presentation/components/input.spec.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { Input } from '@/presentation/components';
 
+const makeSut = () => render(<Input name="field" />);
+
 describe('Input Component', () => {
   test('should begin with empty error', () => {
-    const { getByTitle } = render(<Input name="field" />);
+    const { getByTitle } = makeSut();
     const input = getByTitle('');
     expect(input).toBeTruthy();
   });
