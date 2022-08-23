7ddfedd3a2b976336186fe398c5c1d9153499c1d
ericknathan <erick.capito@hotmail.com>
2022-08-23 10:40:05 -0300

    test: ensure SignIn page begin with correct state

--- __tests__/cypress/e2e/signin.cy.ts
+++ __tests__/cypress/e2e/signin.cy.ts
@@ -1,5 +1,12 @@
 describe('SignIn E2E', () => {
-  it('should load with correct initial state', () => {
+  beforeEach(() => {
     cy.visit('signin');
+  });
+
+  it('should load with correct initial state', () => {
+    cy.getByTestId('email-input').should('have.attr', 'title', '');
+    cy.getByTestId('password-input').should('have.attr', 'title', '');
+    cy.getByTestId('signin-button').should('have.attr', 'disabled');
+    cy.getByTestId('main-error-message').should('not.have.text');
   });
 });
