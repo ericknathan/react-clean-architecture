9651be35f6c50e9633908d6361a086e23b0160b0
ericknathan <erick.capito@hotmail.com>
2022-08-23 10:53:57 -0300

    test: ensure SignIn page present error state if form is invalid

--- __tests__/cypress/e2e/signin.cy.ts
+++ __tests__/cypress/e2e/signin.cy.ts
@@ -1,11 +1,21 @@
+import { faker } from '@faker-js/faker/locale/pt_BR';
+
 describe('SignIn E2E', () => {
   beforeEach(() => {
     cy.visit('signin');
   });
 
   it('should load with correct initial state', () => {
+    cy.getByTestId('email-input').should('have.attr', 'autocomplete');
     cy.getByTestId('email-input').should('have.attr', 'title', '');
     cy.getByTestId('password-input').should('have.attr', 'title', '');
+    cy.getByTestId('signin-button').should('have.attr', 'disabled');
+    cy.getByTestId('main-error-message').should('not.have.text');
+  });
+
+  it('should present error state if form is invalid', () => {
+    cy.getByTestId('email-input').focus().type(faker.random.word()).should('have.attr', 'title', 'Campo e-mail inválido.');
+    cy.getByTestId('password-input').focus().type(faker.random.alphaNumeric(3)).should('have.attr', 'title', 'O valor de senha deve ter no mínimo 5 caracteres.');
     cy.getByTestId('signin-button').should('have.attr', 'disabled');
     cy.getByTestId('main-error-message').should('not.have.text');
   });
