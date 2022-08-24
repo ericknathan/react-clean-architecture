f96bedb29196e16390a159b68be04591a9d1f9f4
ericknathan <erick.capito@hotmail.com>
2022-08-24 11:08:01 -0300

    test: ensure SignUp page present error state if form is invalid

--- __tests__/cypress/integration/signup.cy.ts
+++ __tests__/cypress/integration/signup.cy.ts
@@ -1,3 +1,4 @@
+import { faker } from '@faker-js/faker/locale/pt_BR';
 import { FormHelper } from '@/tests/cypress/support/helpers';
 
 describe('SignIn Integration', () => {
@@ -11,6 +12,14 @@ describe('SignIn Integration', () => {
     FormHelper.testInputStatus('email-input', '');
     FormHelper.testInputStatus('password-input', '');
     FormHelper.testInputStatus('password-confirmation-input', '');
+    FormHelper.testDisabledButton('signup-button', 'main-error-message');
+  });
+
+  it('should present error state if form is invalid', () => {
+    FormHelper.insertTextAndTestInputStatus('name-input', faker.random.alphaNumeric(2), 'O valor de nome deve ter no mínimo 3 caracteres.')
+    FormHelper.insertTextAndTestInputStatus('email-input', faker.random.word(), 'Campo e-mail inválido.')
+    FormHelper.insertTextAndTestInputStatus('password-input', faker.random.alphaNumeric(3), 'O valor de senha deve ter no mínimo 5 caracteres.')
+    FormHelper.insertTextAndTestInputStatus('password-confirmation-input', faker.random.alphaNumeric(4), 'Campo confirmação de senha inválido.')
     FormHelper.testDisabledButton('signup-button', 'main-error-message');
   });
 });
