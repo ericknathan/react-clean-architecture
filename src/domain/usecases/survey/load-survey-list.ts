2cccbdffca1ed5d28592269aa43314086ae3a2b7
ericknathan <erick.capito@hotmail.com>
2022-08-29 08:16:05 -0300

    feat: add LoadSurveyList usecase

---
+++ src/domain/usecases/survey/load-survey-list.ts
@@ -0,0 +1,9 @@
+import { Survey } from '@/domain/models';
+
+export interface LoadSurveyList {
+  loadAll(): Promise<LoadSurveyList.Result>
+}
+
+export namespace LoadSurveyList {
+  export type Result = Survey.Model
+}
