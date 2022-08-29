f8aada26ca8d1046ac0cd7c1785d1e0bc3750929
ericknathan <erick.capito@hotmail.com>
2022-08-29 09:50:56 -0300

    test: ensure RemoteLoadSurveyList throws UnexpectedError on 500

--- __tests__/data/usecases/survey/load-survey-list/remote-load-survey-list.spec.ts
+++ __tests__/data/usecases/survey/load-survey-list/remote-load-survey-list.spec.ts
@@ -48,6 +48,15 @@ describe('RemoteLoadSurveyList', () => {
     await expect(promise).rejects.toThrow(new UnexpectedError());
   });
 
+  it('should throw EmailInUseError if HttpClient returns 500', async () => {
+    const { sut, httpGetClientSpy } = makeSut();
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.SERVER_ERROR,
+    };
+    const promise = sut.loadAll();
+    await expect(promise).rejects.toThrow(new UnexpectedError());
+  });
+  
   it('should return a list of SurveyModel if HttpClient returns 200', async () => {
     const { sut, httpGetClientSpy } = makeSut();
     const httpResult = mockSurveyListModel();
