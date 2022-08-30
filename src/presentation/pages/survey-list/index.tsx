8cdbfa10486ae676980b0c8084a6f0b2b2bb50ea
ericknathan <erick.capito@hotmail.com>
2022-08-30 11:20:29 -0300

    feat: add skeleton shimmer effect

--- src/presentation/pages/survey-list/index.tsx
+++ src/presentation/pages/survey-list/index.tsx
@@ -30,27 +30,7 @@ export function SurveyList() {
               <button>Ver resultados</button>
             </footer>
           </li>
-          <li className={styles.questionWrapper}>
-            <div className={[styles.questionData, styles.answeredStatusSuccess].join(' ')}>
-              <time>
-                <span>Em: </span>
-                18/03/2022
-              </time>
-              <h3>O que você usa para desenvolver aplicativos?</h3>
-              <ul className={styles.questionAnswers}>
-                <li>Flutter</li>
-                <li>Nativo</li>
-                <li>Ionic</li>
-                <li>Native Script</li>
-                <li>Phonegap</li>
-                <li>Titanium</li>
-                <li>Xamarin</li>
-              </ul>
-            </div>
-            <footer>
-              <button>Ver resultados</button>
-            </footer>
-          </li>
+          <li className={styles.questionWrapper} />
         </ul>
       </div>
       <Footer />
