f1c8f19ef2afc514d86663202e8f31ddc7b4e6fa
ericknathan <erick.capito@hotmail.com>
2022-08-30 10:22:28 -0300

    feat: add answered status indicator to question

--- src/presentation/pages/survey-list/index.tsx
+++ src/presentation/pages/survey-list/index.tsx
@@ -31,28 +31,7 @@ export function SurveyList() {
             </footer>
           </li>
           <li className={styles.questionWrapper}>
-            <div className={styles.questionData}>
-              <time>
-                <span>Em: </span>
-                18/03/2022
-              </time>
-              <h3>O que você usa para desenvolver aplicativos?</h3>
-              <ul className={styles.questionAnswers}>
-                <li>Flutter</li>
-                <li>Nativo</li>
-                <li>Ionic</li>
-                <li>Native Script</li>
-                <li>Phonegap</li>
-                <li>Titanium</li>
-                <li>Xamarin</li>
-              </ul>
-            </div>
-            <footer>
-              <button>Ver resultados</button>
-            </footer>
-          </li>
-          <li className={styles.questionWrapper}>
-            <div className={styles.questionData}>
+            <div className={[styles.questionData, styles.answeredStatusSuccess].join(' ')}>
               <time>
                 <span>Em: </span>
                 18/03/2022
