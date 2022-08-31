af0923d83de6d4b998de82345c5a305afc12aedf
ericknathan <erick.capito@hotmail.com>
2022-08-31 11:47:59 -0300

    refactor: move SUT creation to a factory method

--- __tests__/presentation/pages/survey-list.spec.tsx
+++ __tests__/presentation/pages/survey-list.spec.tsx
@@ -3,9 +3,13 @@ import { expect } from '@jest/globals';
 import { render, screen } from '@testing-library/react';
 import { SurveyList } from '@/presentation/pages';
 
+const makeSut = (): void => {
+  render(<SurveyList />);
+};
+
 describe('SurveyList Page', () => {
   it('should present four empty items on start', () => {
-    render(<SurveyList />);
+    makeSut();
     const surveyList = screen.getByTestId('survey-list');
     expect(surveyList.querySelectorAll('li:empty').length).toBe(4);
   });
