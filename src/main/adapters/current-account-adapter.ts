98930f4c17575c7c078c7c2b0d67f2425297abc9
ericknathan <erick.capito@hotmail.com>
2022-08-31 09:57:32 -0300

    feat: add getCurrentAccountAdapter

--- src/main/adapters/current-account-adapter.ts
+++ src/main/adapters/current-account-adapter.ts
@@ -1,10 +1,14 @@
 import { UnexpectedError } from "@/domain/errors";
 import { Account } from "@/domain/models";
-import { makeLocalStorageAdapter } from "../factories/cache";
+import { makeLocalStorageAdapter } from "@/main/factories/cache";
 
 export function setCurrentAccountAdapter(account: Account.Model): void {
   if(!account?.accessToken) {
     throw new UnexpectedError();
   }
   makeLocalStorageAdapter().set('@4devs/account', account);
+}
+
+export function getCurrentAccountAdapter(): void {
+  return makeLocalStorageAdapter().get('@4devs/account');
 }
