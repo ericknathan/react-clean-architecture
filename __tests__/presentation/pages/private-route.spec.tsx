6751a5626bf097df06b3738d69f68e2cfd550bc5
ericknathan <erick.capito@hotmail.com>
2022-08-31 10:31:06 -0300

    test: ensure PrivateRoute renders component if token is not empty

--- __tests__/presentation/pages/private-route.spec.tsx
+++ __tests__/presentation/pages/private-route.spec.tsx
@@ -4,17 +4,22 @@ import { render } from '@testing-library/react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory, MemoryHistory } from "history";
 import { PrivateRoute } from '@/presentation/pages';
+import { ApiContext } from '@/presentation/contexts';
+import { mockAccountModel } from '@/../__mocks__/domain';
+import { Account } from 'domain/models';
 
 type SutTypes = {
   history: MemoryHistory;
 }
 
-const makeSut = (): SutTypes => {
+const makeSut = (account: Account.Model | null = mockAccountModel()): SutTypes => {
   const history = createMemoryHistory({ initialEntries: ['/'] });
   render(
-    <Router location={history.location} navigator={history}>
-      <PrivateRoute />
-    </Router>
+    <ApiContext.Provider value={{ getCurrentAccount: () => account }}>
+      <Router location={history.location} navigator={history}>
+        <PrivateRoute />
+      </Router>
+    </ApiContext.Provider>
   );
 
   return { history };
@@ -22,7 +27,12 @@ const makeSut = (): SutTypes => {
 
 describe('PrivateRoute', () => {
   it('should redirect to /signin if token is empty', () => {
-    const { history } = makeSut();
+    const { history } = makeSut(null);
     expect(history.location.pathname).toBe('/signin');
+  });
+
+  it('should render current component if token is not empty', () => {
+    const { history } = makeSut();
+    expect(history.location.pathname).toBe('/');
   });
 });
