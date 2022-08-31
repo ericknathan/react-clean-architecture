e1c0aca1fe2b1e32c14bedb325a7b5f174efbbfc
ericknathan <erick.capito@hotmail.com>
2022-08-31 11:54:18 -0300

    test: ensure SurveyList component calls LoadSurveyList

--- __tests__/presentation/pages/survey-list.spec.tsx
+++ __tests__/presentation/pages/survey-list.spec.tsx
@@ -2,15 +2,40 @@ import React from 'react';
 import { expect } from '@jest/globals';
 import { render, screen } from '@testing-library/react';
 import { SurveyList } from '@/presentation/pages';
+import { LoadSurveyList } from '@/domain/usecases';
 
-const makeSut = (): void => {
-  render(<SurveyList />);
+class LoadSurveyListSpy implements LoadSurveyList {
+  callsCount = 0;
+  
+  loadAll(): Promise<LoadSurveyList.Result> {
+    this.callsCount++;
+    return null!;
+  }
+}
+
+type SutTypes = {
+  loadSurveyListSpy: LoadSurveyListSpy;
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyListSpy = new LoadSurveyListSpy();
+  render(<SurveyList loadSurveyList={loadSurveyListSpy} />);
+
+  return { 
+    loadSurveyListSpy
+  };
 };
 
 describe('SurveyList Page', () => {
   it('should present four empty items on start', () => {
     makeSut();
+
     const surveyList = screen.getByTestId('survey-list');
     expect(surveyList.querySelectorAll('li:empty').length).toBe(4);
+  });
+
+  it('should call LoadSurveyList', () => {
+    const { loadSurveyListSpy } = makeSut();
+    expect(loadSurveyListSpy.callsCount).toBe(1);
   });
 });
