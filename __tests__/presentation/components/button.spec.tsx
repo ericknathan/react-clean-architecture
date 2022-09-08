07086684d2b932d40dad274b9bee57488fa8d0c7
ericknathan <erick.capito@hotmail.com>
2022-09-08 09:35:17 -0300

    refactor: rename test function

--- __tests__/presentation/components/button.spec.tsx
+++ __tests__/presentation/components/button.spec.tsx
@@ -6,7 +6,7 @@ import { Button } from '@/presentation/components';
 const makeSut = () => render(<Button>example</Button>);
 
 describe('Button Component', () => {
-  test('should begin with empty className', () => {
+  it('should begin with empty className', () => {
     const { getByTestId } = makeSut();
     const button = getByTestId('button');
     expect(button.className).not.toContain('undefined');
--- __tests__/presentation/components/input.spec.tsx
+++ __tests__/presentation/components/input.spec.tsx
@@ -6,7 +6,7 @@ import { Input } from '@/presentation/components';
 const makeSut = () => render(<Input name="field" />);
 
 describe('Input Component', () => {
-  test('should begin with empty error', () => {
+  it('should begin with empty error', () => {
     const { getByTitle } = makeSut();
     const input = getByTitle('');
     expect(input).toBeTruthy();
