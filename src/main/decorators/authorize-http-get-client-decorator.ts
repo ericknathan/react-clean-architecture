f79625c8e9bf7ac05f16df3599c527698066cff2
ericknathan <erick.capito@hotmail.com>
2022-09-08 08:57:59 -0300

    feat: ensure AuthorizeHttpGetClient Decorator returns the same as HttpGetClient

--- src/main/decorators/authorize-http-get-client-decorator.ts
+++ src/main/decorators/authorize-http-get-client-decorator.ts
@@ -16,6 +16,7 @@ export class AuthorizeHttpGetClientDecorator implements HttpGetClient {
         })
       });
     }
-    return this.httpGetClient.get(params);
+    const httpResponse = this.httpGetClient.get(params);
+    return httpResponse;
   }
 }
